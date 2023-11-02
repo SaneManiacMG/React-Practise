878838330994bb56f61ebfd214a8258ca7e8203d
SaneManiacMG <mmoroane@hotmail.com>
2023-11-02 11:28:49 +0200

    Portal demo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from "react";
-import Greet from "./components/Greet";
-import Counter from "./components/Counter";
-import Message from "./components/Message";
-import EventHandling from "./components/EventHandling";
-import ParentComponent from "./components/ParentComponent";
-import UserGreeting from "./components/UserGreeting";
-import NameList from "./components/NameList";
-import Stylesheet from "./components/Stylesheet";
 import "./App.css";
-import Inline from "./components/Inline";
 import "./appStyles.css";
-import styles from "./appStyles.module.css";
-import Form from "./components/Form";
-import FragmentDemo from "./components/FragmentDemo";
-import Table from "./components/Table";
-import ParentComp from "./components/ParentComp";
-import RefsDemo from "./components/RefsDemo";
+import PortalDemo from "./components/PortalDemo";
 
-function App() {
+const App = () => {
 	return (
 		<>
 			<p>Test output</p>
-			<RefsDemo />
+			<PortalDemo />
 		</>
 	);
-}
+};
 
 export default App;
